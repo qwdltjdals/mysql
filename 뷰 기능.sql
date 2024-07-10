5b30497a45a99c5ffad32ca6d29eaff0aa857050
qwdltjdals <iu_love@kakao.com>
2024-07-10 09:21:12 +0900

    자리옮기기전

---
+++ 뷰 기능.sql
@@ -0,0 +1,28 @@
+# view (뷰) 가상 테이블
+
+create view dvd_view as
+select
+	dt.dvd_id,
+    dt.registration_number,
+    dt.title,
+    dt.producer_id,dvd_view
+    pdt.producer_name,
+    dt.publisher_id,
+    pbt.publisher_name,
+    dt.publication_year,
+    dt.database_date
+from
+	dvd_tb dt
+    left outer join producer_tb pdt on(pdt.producer_id = dt.producer_id)
+    left outer join publisher_tb pbt on(pbt.publisher_id = dt.publisher_id);
+    
+    
+    
+    
+    
+select
+	*
+from
+	dvd_view
+where
+	producer_id = 10;
---
+++ 트랜젝션.sql
@@ -0,0 +1,48 @@
+# 트랜젝션(Transaction)
+select @@autocommit; -- @@ 전역세팅
+# rollback을 하게되면 start transaction 다시 해야함
+
+start transaction; # = 중괄효 연 것 효과
+
+set @name = "권혁진";
+set @phone = "010-6666-5555";
+
+savepoint sp_insert_name;
+insert into join1_tb
+values(0, @name);
+
+savepoint sp_insert_phone;
+insert into join2_tb
+values(0, @phone);
+
+rollback to sp_insert_phone;
+
+set @new_join1_id = 0;
+set @new_join2_id = 0;
+
+select
+	join1_id into @new_join1_id
+from
+	join1_tb
+order by
+	join1_id desc
+limit 0, 1;
+
+select @new_join1_id;
+
+select
+	join2_id into @new_join2_id
+from
+	join2_tb
+order by
+	join2_id desc
+limit 0,1;
+
+insert into master_tb
+values(0, @new_join1_id, @new_join2_id);
+
+commit;
+
+select
+	@new_join1_id,
+    @new_join2_id;
